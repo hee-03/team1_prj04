53ad83b30c1a5ce61962a3019e8d169161c31838
hoilove304-droid <hoilove304@gmail.com>
2025-12-17 12:04:23 +0900

    mytrip page 실행 css header, footer 수정 값 적용

--- prj04/src/main/java/web/com/prj04/a02_mvc/A01_MVC_Controller.java
+++ prj04/src/main/java/web/com/prj04/a02_mvc/A01_MVC_Controller.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class A01_MVC_Controller {
@@ -21,4 +23,12 @@ public class A01_MVC_Controller {
 		return "deptList";
 		
 	}
+	
+	
+	// http://localhost:6060/mytrip
+	@GetMapping("mytrip")
+	public String mytrip() {
+		return "sejin/mytrip";
+	}
+	
 }
