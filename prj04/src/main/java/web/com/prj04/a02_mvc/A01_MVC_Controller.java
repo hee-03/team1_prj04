c577c8643256e6998c134575c953854eb3e6b90c
heejeong park <oeo223@gmail.com>
2025-12-17 17:44:22 +0900

    rec_travel event commit

--- prj04/src/main/java/web/com/prj04/a02_mvc/A01_MVC_Controller.java
+++ prj04/src/main/java/web/com/prj04/a02_mvc/A01_MVC_Controller.java
@@ -13,32 +13,24 @@ public class A01_MVC_Controller {
 	@Autowired
 	private A02_MVC_Service service;
 	
-	// http://localhost:6060/main
+	// http://localhost:7070/main
 	@GetMapping("main")
 	public String main() {
 		return "index";
 	}
 	
-	// http://localhost:6060/mytrip
+	// http://localhost:7070/mytrip
 	@GetMapping("mytrip")
 	public String mytrip() {
 		return "mytrip";
 	}
 
-	// http://localhost:6060/rec_travel
+	// http://localhost:7070/rec_travel
 		@GetMapping("rec_travel")
 		public String rec_travel() {
 			return "rec_travel";
 		}
 	
-		
-		
-		
-		
-		
-		
-		
-		
 		
 		// Model : 핵심
 		// http://localhost:6060/deptList
